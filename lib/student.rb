21b84499e338cd3d2b6e1e10fafaaa145a7b3a1f
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-17 15:25:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,8 +89,12 @@ class Student
      SELECT * 
      FROM students
      WHERE grade = 10
+     LIMIT ?
    SQL
-    
+  
+    DB[:conn].execute(sql,num).collect do |row|
+      self.new_from_db(row)
+    end  
     
     
   end   
