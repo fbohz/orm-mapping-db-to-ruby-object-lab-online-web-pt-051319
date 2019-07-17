d2e944f6d1f3597e9df8d08a2701c087a068d396
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-17 15:21:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,11 +72,11 @@ class Student
     end 
   end   
   
-  def.self.students_below_12th_grade 
+  def self.students_below_12th_grade 
    sql = <<-SQL
      SELECT * 
      FROM students
-     WHERE grade < 9
+     WHERE grade < 12
    SQL
   
     DB[:conn].execute(sql).collect do |row|
